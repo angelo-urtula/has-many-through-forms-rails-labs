1c978b06ba312acd30a59f67fecbca5fa47acc0d
angelo <aurtula1@gmail.com>
2021-05-18 14:20:20 -0400

    commit tree 0e27f02b4a6c4a38ac1f63826aeba577cb65dd87

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    user = User.create(user_params)
+    redirect_to user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
 end
